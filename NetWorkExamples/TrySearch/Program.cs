5a278f88c934b19278545e3c74545d73e918c291
rpe4a <zapman2la@gmail.com>
2017-08-15 14:12:54 +0500

    add search algoritms

---
+++ NetWorkExamples/TrySearch/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TrySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Enumerable.Range(1, 1000).ToArray();
+
+            DoubleFindArray(array, 501);
+        }
+
+        static void LineFind(int[] array, int findValue)
+        {
+            var count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Проход {count++}: ");
+
+                if (array[i] == findValue)
+                    break;
+
+            }
+        }
+
+        static void DoubleFind(int[] array, int findValue)
+        {
+            var count = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Проход {count++}: ");
+
+                var t = array[array.Length/2];
+
+                if (t == findValue)
+                    break;
+
+                if (t < findValue)
+                    array = array.Skip(array.Length / 2).ToArray();
+                else
+                    array = array.Take(array.Length / 2).ToArray();
+
+
+            }
+        }
+        static void DoubleFindArray(int[] array, int findValue)
+        {
+            var count = 0;
+
+            var first = 0;
+
+            var last = array.Length;
+
+            while (first < last)
+            {
+                Console.WriteLine($"Проход {count++}: ");
+
+                var mid = first + (last - first)/2;
+
+                if (findValue <= array[mid])
+                    last = mid;
+                else
+                    first = mid + 1;
+
+            }
+
+            if (array[last] == findValue)
+                Console.WriteLine("Finded");
+        }
+
+        static void ToStringArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- NetWorkExamples/TrySort/Program.cs
+++ NetWorkExamples/TrySort/Program.cs
@@ -10,18 +10,21 @@ namespace TrySort
     {
         static void Main(string[] args)
         {
-            var array = new[] { 1, 9, 4, 5, 6, 42, 28, 3, 13,15, 26, 8, 2, 34, 7, 321 };
+            var array = new[] { 1, 9, 4, 5, 6, 42, 28, 3, 13, 15 };
 
             ToStringArray(array);
 
             Console.WriteLine($"Сортируем массив {nameof(array)}");
 
-            ToStringArray(BubbleSort(array));
+            //ToStringArray(BubbleSort(array));
+            ToStringArray(InsertionSort(array));
 
         }
 
         static int[] BubbleSort(int[] array)
         {
+            var count = 0;
+
             for (int i = 0; i < array.Length; i++)
             {
                 for (int j = 0; j < array.Length - 1; j++)
@@ -33,11 +36,34 @@ namespace TrySort
                         array[j] = array[j] - array[j + 1];
                     }
 
+                    Console.Write($"Проход {count++}: ");
                     ToStringArray(array);
                 }
             }
 
             return array;
+        }
+
+        static int[] InsertionSort(int[] array)
+        {
+            var count = 0;
+
+            int[] result = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && result[j - 1] > array[i])
+                {
+                    result[j] = result[j - 1];
+                    j--;
+
+                    Console.Write($"Проход {count++}: ");
+                    ToStringArray(result);
+
+                }
+                result[j] = array[i];
+            }
+            return result;
         }
 
         static void ToStringArray(int[] array)
