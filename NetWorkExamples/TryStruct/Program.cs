79f2a4bedb1452647580fb9e70fff595b279e852
rpe4a <zapman2la@gmail.com>
2017-09-27 00:04:13 +0500

    Add Struct Sample

---
+++ NetWorkExamples/TryStruct/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryStruct
+{
+    struct Point2D:IEquatable<Point2D>
+    {
+        private List<int> a;
+        public int X;
+        public int Y;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point2D))
+            {
+                return false;
+            }
+
+            var d = (Point2D)obj;
+            return X == d.X && Y == d.Y;
+        }
+
+        public bool Equals(Point2D point2D)
+        {
+            return X == point2D.X && Y == point2D.Y;
+        }
+
+        public static bool operator ==(Point2D d1, Point2D d2)
+        {
+            return d1.Equals(d2);
+        }
+
+        public static bool operator !=(Point2D d1, Point2D d2)
+        {
+            return !(d1 == d2);
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
