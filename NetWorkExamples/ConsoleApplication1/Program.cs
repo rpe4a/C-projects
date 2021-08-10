ed80345ee42f74f326ee7c880bebddeba138f539
rpe4a <zapman2la@gmail.com>
2021-08-11 00:26:33 +0500

    Dijkstra

--- NetWorkExamples/ConsoleApplication1/Program.cs
+++ NetWorkExamples/ConsoleApplication1/Program.cs
@@ -1,108 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace AlgoritmicExamples
+namespace ConsoleApp1
 {
     class Program
     {
-
-        static bool Beetree(int x1, int y1, int x2, int y2)
-        {
-            bool result = false;
-
-            if (x1 + y1 >= x2 + y2)
-                result = false;
-
-            if (x1 + y1 < x2 + y2)
-            {
-                if ((x2 == x1 + y1 && y1 == y2) || (x2 == x1 && y2 == x1 + y1))
-                {
-                    result = true;
-                }
-                else
-                {
-                    if(Beetree(x1 + y1, y1, x2, y2))
-                        result = true;
-                    else
-                        result = Beetree(x1 , x1+ y1, x2, y2);
-                }
-            }
-
-            return result;
-        }
+        private const string Start = "start";
+        private const string A = "a";
+        private const string B = "b";
+        private const string Finish = "finish";
+        private const string None = "none";
 
         static void Main(string[] args)
         {
-            int x1 = 1, y1 = 4, x2 = 7, y2 = 6;
-            int xResult=x1, yResult=y1;
+            var graph = new Dictionary<string, Dictionary<string, int>>();
+            graph[Start] = new Dictionary<string, int>();
+            graph[Start][A] = 6;
+            graph[Start][B] = 2;
+            graph[A] = new Dictionary<string, int>();
+            graph[A][Finish] = 1;
+            graph[B] = new Dictionary<string, int>();
+            graph[B][A] = 3;
+            graph[B][Finish] = 5;
+            graph[Finish] = new Dictionary<string, int>();
 
-            
-            
-            
+            var costs = new Dictionary<string, int>();
+            costs[A] = 6;
+            costs[B] = 2;
+            costs[Finish] = int.MaxValue;
 
-            Console.WriteLine((Beetree(x1, y1, x2, y2)) ? "Yes" : "No");
+            var parents = new Dictionary<string, string>();
+            parents[A] = Start;
+            parents[B] = Start;
+            parents[Finish] = None;
 
+            var processed = new List<string>(graph.Count);
 
-            //string[] arrStr = new string[] {"<<<><><>"};
-            //int[] maxRep = new int[] {2};
-            //var arrResult = new int[arrStr.Length];
+            //Алгоритм Дейкстры
+            var node = FindLowestCostNode(costs, processed);
+            while (node != None)
+            {
+                var cost = costs[node];
+                var neighbors = graph[node];
+                foreach (var key in neighbors.Keys)
+                {
+                    var new_cost = cost + neighbors[key];
+                    if (costs[key] > new_cost)
+                    {
+                        costs[key] = new_cost;
+                        parents[key] = node;
+                    }
+                }
 
-            //for (int i = 0; i < arrStr.Length; i++)
-            //{
+                processed.Add(node);
+                node = FindLowestCostNode(costs, processed);
+            }
+        }
 
-            //    //var str = arrStr[i];
-            //    //for (int j = 0; j < str.Length - 1; j++)
-            //    //{
-            //    //    char l = str[j];
-            //    //    char r = str[j + 1];
-            //    //    if (l == r)
-            //    //        str = r == '<' ? '>' : r;
-            //    //}
+        private static string FindLowestCostNode(Dictionary<string, int> costs, List<string> processed)
+        {
+            var lowest_cost = int.MaxValue;
+            var lowest_cost_node = None;
 
-            //    if (new[] {">>"}.Contains(arrStr[i]))
-            //    {
-            //        arrResult[i] = 0;
-            //        continue;
-            //    }
+            foreach (var key in costs.Keys)
+            {
+                var cost = costs[key];
+                if (cost < lowest_cost && !processed.Contains(key))
+                {
+                    lowest_cost = cost;
+                    lowest_cost_node = key;
+                }
+            }
 
-            //    var str = arrStr[i].Replace("<>", "");
-
-
-            //    var lSymbol = str.Count(x => x.Equals('<'));
-            //    var rSymbol = str.Count(x => x.Equals('>'));
-
-            //    if (lSymbol > 0)
-            //    {
-            //        arrResult[i] = 0;
-            //        continue;
-            //    }
-
-            //    if(rSymbol <= maxRep[i])
-            //        arrResult[i] = 1;
-            //    else
-            //        arrResult[i] = 0;
-
-            //}
-
-            //foreach (var i in arrResult)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            return lowest_cost_node;
         }
     }
 }
-
-
-//var result = Math.Abs(lSymbol - rSymbol);
-
-//                if (result == 0 && maxRep[i] > 0)
-//                {
-//                    arrResult[i] = 1;
-//                    continue;
-//                }
-
-//                arrResult[i] = maxRep[i] != 0 && result == maxRep[i] ? 1 : 0;
